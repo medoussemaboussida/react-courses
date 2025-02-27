c8dcff82fd9a2d5ea05594584bee70b1b8588927
med oussema boussida <medoussemaboussida@gmail.com>
2025-02-27 10:22:39 +0100

    routage

--- workshop-react/src/App.jsx
+++ workshop-react/src/App.jsx
@@ -1,66 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
 import './App.css'
-import Hello from './Component/Hello'
-import Form from './Component/Form'
-import Button from 'react-bootstrap/Button';
-import ComponentClass from './Component/ComponentClass'
-import ComponentFonct from './Component/ComponentFile'
-import Counter from './Component/useEffect'
-import Timer from './Component/tableauUseEffect'
-import ColorBox from './Component/Color'
-import Events from './Component/Events'
-import Products from './Component/Products'
+
+// import NotFound from './Component/NotFound'
+// Lazy loading des composants
+const Events = React.lazy(() => import("./Component/Events"));
+const Products = React.lazy(() => import("./Component/Products"));
+const NotFound = React.lazy(() => import("./Component/NotFound"));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/* <div>
+    <Router>
+      <nav>
+        <Link to="/home">Home</Link> | <Link to="/jjj">Test 404</Link>
+      </nav>
 
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      {/* <Hello />
-      <Form labelle="First Name :" name="first name" type="text" placeholder="first name"/>
-      <Form labelle="Last Name :" name="first name" type="text" placeholder="last name"/> */}
-      {/* <div className="App">
-      <ComponentClass />
-    </div> */}
-    {/* <ComponentFonct/>
-    <Counter/>
-    <Timer/>
-    <ColorBox/> */}
-     <div className="App">
-      <h1 className="text-center my-4">Event Management System</h1>
-      <Events/>
-    </div>
-     <div className="App">
-      <h1 className="text-center my-4">Product</h1>
-      <Products/>
-    </div>
-    </>
-    
-
-  )
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <div className="App">
+                  <h1 className="text-center my-4">Event Management System</h1>
+                  <Events />
+                </div>
+                <div className="App">
+                  <h1 className="text-center my-4">Product</h1>
+                  <Products />
+                </div>
+              </>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
-export default App
+export default App;
